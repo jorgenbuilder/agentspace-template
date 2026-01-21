e58eb9fc78789b263d6d41ec2d969d764af268e3
jorgenbuilder <jorgen@buildnode.io>
2026-01-21 14:19:13 -0800

    i skills

--- .devcontainer/scripts/60-install-skills.sh
+++ .devcontainer/scripts/60-install-skills.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "==> Installing skills..."
 
+npm i skills
 npx skills add vercel-labs/agent-skills -s "vercel-react-best-practices" -a claude-code -g -y
 npx skills add vercel-labs/agent-skills -s "web-design-guidelines" -a claude-code -g -y
 npx skills add vercel-labs/agent-browser -s "agent-browser" -a claude-code -g -y
